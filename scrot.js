fee0e133cd2f3ef1c4a632932434b3e1823fa99f
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-12-08 12:56:32 -0500

    Working on a faster version that handles 2 pages at a time.

--- scrot.js
+++ scrot.js
@@ -5,7 +5,7 @@ var scrtDelay = 200;
 
 var fs = require('fs');
 
-function scrot(url, file, onDone, x) {
+function scrot(url, file, onDone, x, firstHalf) {
    var page = require('webpage').create();
 
    page.open(url, function(status) {
@@ -15,7 +15,7 @@ function scrot(url, file, onDone, x) {
          page.render(file);
 
       page.close();
-      onDone(x);
+      onDone(x, firstHalf);
       //phantom.exit();
    });
 }
@@ -48,27 +48,57 @@ function url_lst_scrot(links_path) {
          phantom.exit();
    }*/
 
-   function rec_scrt(i) {
-      if (i >= max_scrots || !(i < links_lst.length)) {
+   /*var rec1_start = 0;
+   var rec1_end = 15;
+   var rec2_start = 15;
+   var rec2_end = 30;*/
+   //var rec_start = null, rec_end = null;
+   var rec1_end = 15;
+   var rec2_end = 30;
+
+   var first_done = false, second_done = false;
+
+   function rec_scrt(i, first_half) {
+      var local_max_scrots = first_half ? rec1_end : rec2_end;
+      if (i >= local_max_scrots || !(i < links_lst.length)) {
+         if (first_half) first_done = true;
+         if (!first_half) second_done = true;
+
          //waitTillDone();
-         phantom.exit();
+         if (first_done && second_done)
+            phantom.exit();
          return;
       }
 
       //console.log(links_lst.length);
       var url = links_lst[i];
-      console.log(url);
+      console.log('i:', i, 'url:', url);
       statuses.push(false);
 
       //function onDone(x) { statuses[x] = true; }
-      scrot(url,  'scrots/file' + i.toString() + '.png', rec_scrt, i+1);//onDone, i);
+      scrot(url,  'scrots/file' + i.toString() + '.png', rec_scrt, i+1, first_half);//onDone, i);
 
       //setTimeout(rec_scrt, scrtDelay, i+1);
    }
 
-   rec_scrt(0);
+
+   rec_scrt(0, true);
+   rec_scrt(15, false);
    //waitTillDone();
 }
 
 url_lst_scrot('links.txt.lst');
 
+
+/*1 at a time:
+real  0m26.185s
+user  0m6.704s
+sys   0m0.620s
+
+
+2 at a time:
+real  0m19.300s
+user  0m6.692s
+sys   0m0.656s
+
+*/
